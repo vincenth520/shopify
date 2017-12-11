224ee5ffeebc61b4ac1490720fb35287341774e5
vincenth520 <1091986039@qq.com>
2017-12-11 17:32:56 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # Shopify
+
+## Setup/Installation
+You can install it on packgist
+`composer require donutdan4114/shopify`
+
+
+You must first setup a public app. View documentation. You need an authorization URL.
+
+```php
+require 'vendor/autoload.php';
+session_start();
+$APP_API_KEY = ''; //api key
+$APP_SECRET = ''; //app secret
+
+$client = new Shopify\Shopify($_GET['shop'], $APP_API_KEY, $APP_SECRET);
+$_SESSION['client_id'] = time();
+$random_state = $_SESSION['client_id'];
+
+$rediect_url = 'http://localhost/shopify/shopify/redirect.php';  //this is your redirect url
+
+$client->authorizeUser($rediect_url, [
+  'read_products',
+  'write_products',
+], $random_state);
+
+```
+
+At this point, the user is taken to their store to authorize the application to use their information.
+If the user accepts, they are taken to the redirect URL.
+
+```php
+session_start();
+require 'vendor/autoload.php';
+$APP_API_KEY = ''; //api key
+$APP_SECRET = ''; //app secret
+
+
+$client = new Shopify\Shopify($_GET['shop'], $APP_API_KEY, $APP_SECRET);
+$client->setState($_SESSION['client_id']);
+if ($token = $client->getAccessToken()) {
+  $_SESSION['shopify_access_token'] = $token;
+  $_SESSION['shopify_shop_domain'] = $_GET['shop'];
+}
+else {
+  die('invalid token');
+}
+```
+
+### Webhooks
+
+- get shop info
+```php
+$client = new Shopify\Shopify($_SESSION['shopify_shop_domain'], $APP_API_KEY, $APP_SECRET);
+$client->getShopInfo();
+```
+
+- get webhook
+```php
+$client = new Shopify\Shopify($_SESSION['shopify_shop_domain'], $APP_API_KEY, $APP_SECRET);
+$client->getWebhooks();
+```
+
+-add webhook
+```php
+$client = new Shopify\Shopify($_SESSION['shopify_shop_domain'], $APP_API_KEY, $APP_SECRET);
+$type = 'collections/create'; //topics  [see:https://help.shopify.com/api/reference/webhook]
+$address = 'https://baidu.com'; //address URIs
+$format = 'json'; //json or xml
+$client->addWebhook($type,$address,$format);
+```
--- src/Shopify.php
+++ src/Shopify.php
@@ -2,7 +2,7 @@
 
 namespace Shopify;
 use GuzzleHttp\Client;
-
+use GuzzleHttp\RequestOptions;
 
 /**
 * class Shopify
@@ -11,26 +11,45 @@ class Shopify
 {
 	
 	const AUTHORIZE_URL_FORMAT = 'https://{shop_domain}/admin/oauth/authorize?client_id={api_key}&scope={scopes}&redirect_uri={redirect_uri}&state={state}';
-	const ACCESS_TOKEN_URL_FORMAT = 'https://{shop_domain}/admin/oauth/access_token';
+	const ACCESS_TOKEN_URL_FORMAT = 'admin/oauth/access_token';
 	protected $shop_domain;
 	protected $api_key;
 	protected $app_secret;
   	private $access_token = '';
   	private $code;
   	private $state = '';
+  	public $base_url;
+
+
 	
 
 	function __construct($shop_domain, $api_key, $app_secret)
 	{
 		$this->shop_domain = $shop_domain;
+		$this->base_url = 'https://'.$shop_domain;
 		$this->api_key = $api_key;
 		$this->app_secret = $app_secret;
-		$this->http = new Client();
+		$this->http = new Client([
+			'verify'  =>  false,
+			'base_uri' => $this->base_url
+		]);
 	}
 
 	public function setState($state)
 	{
 		$this->state = $state;
+	}
+
+	public function setAccessToken($token)
+	{
+		$this->access_token = $token;
+		$this->http = new Client([
+			'verify'  =>  false,
+			'base_uri' => $this->base_url,
+			'headers' => [
+	            'X-Shopify-Access-Token' => $this->access_token
+	        ]
+	    ]);		
 	}
 
 	public function getAccessToken($refresh = FALSE) {
@@ -47,20 +66,8 @@ class Shopify
 			'code' => $this->code,
 		];
 
-		$domain_path = strtr(self::ACCESS_TOKEN_URL_FORMAT, ['{shop_domain}' => $this->shop_domain]);
-
-		try {
-			$response = $this->http->post( $domain_path,[
-				'form_params' => $data,
-		        'headers' => [
-		            'encoding' => 'application/x-www-form-urlencoded'
-		        ],
-		        'verify'  =>  false
-			]);
-		} catch (ClientException $e) {
-		  return FALSE;
-		}
-		$token = json_decode((string) $response->getBody(),true);
+		$token = $this->responseFormPost(self::ACCESS_TOKEN_URL_FORMAT,$data);
+		$this->setAccessToken($token['access_token']);
 		return $token['access_token'];
 	}
 
@@ -93,6 +100,11 @@ class Shopify
 		return TRUE;
 	}
 
+	public function getResponseBody($response)
+	{
+		return json_decode((string) $response->getBody(),true);
+	}
+
 	public function authorizeUser($redirect_uri, array $scopes, $state) {
 		$url = $this->formatAuthorizeUrl($this->shop_domain, $this->api_key, $scopes, $redirect_uri, $state);
 		header("Location: $url");
@@ -107,6 +119,115 @@ class Shopify
 			'{redirect_uri}' => urlencode($redirect_uri),
 			'{state}' => $state,
 		]);
+	}
+
+	/**
+	 * getProducts
+	 *
+	 * @return Array
+	 * @author vincenth520
+	 **/
+	public function getProducts()
+	{
+		return $this->returnGetPublicData('/admin/products.json');
+	}
+
+	/**
+	 * getShopInfo
+	 *
+	 * @return Array
+	 * @author vincenth520
+	 **/
+	public function getShopInfo()
+	{		
+		return $this->returnGetPublicData('/admin/shop.json');
+	}
+
+	/**
+	 * getWebhooks
+	 *
+	 * @return Array
+	 * @author vincenth520
+	 **/
+	public function getWebhooks()
+	{
+		return $this->returnGetPublicData('/admin/webhooks.json');
+	}
+
+	/**
+	 * addWebhook
+	 *
+	 * add webhook
+	 * 
+	 * @return Array
+	 * @author 
+	 **/
+	public function addWebhook($type,$address,$format='json')
+	{
+		$data_json = [
+			'webhook' => [
+				'topic' => $type,
+				'address' => $address,
+				'format' => $format
+			]
+		];
+		return $this->responseJsonPost('/admin/webhooks.json',$data_json);
+	}
+
+
+	public function responseJsonPost($url,$data)
+	{
+		$body = [
+			RequestOptions::JSON => $data,
+	        'headers' => [
+	            'encoding' => 'application/json'
+	        ]
+	    ];
+	    return $this->returnPostPublicData($url,$body);
+	}
+
+	public function responseFormPost($url,$data)
+	{
+		$body = [
+			'form_params' => $data,
+	        'headers' => [
+	            'encoding' => 'application/x-www-form-urlencoded'
+	        ]
+	    ];
+	    return $this->returnPostPublicData($url,$body);
+	}
+
+
+	/**
+	 * returnPostPublicData
+	 * 
+	 * @return Array
+	 * @author 
+	 **/
+	public function returnPostPublicData($url,$body)
+	{	
+		try {			
+			$response = $this->http->post($url,$body);	
+		} catch (ClientException $e) {
+		  return FALSE;
+		}
+		return $this->getResponseBody($response);
+	}
+
+	/**
+	 * returnGetPublicData
+	 * @params url
+	 * @return Array
+	 * @author 
+	 **/
+	public function returnGetPublicData($url)
+	{
+		try {			
+			$response = $this->http->get($url);
+		} catch (ClientException $e) {
+		  return FALSE;
+		}
+		return $this->getResponseBody($response);
 	}
 
 }
